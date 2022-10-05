ae96d7a0db1d000ab4bc7708e65f11fec8040ec1
TomJ <tom.johansson@yh.nackademin.se>
2022-10-05 22:25:50 +0200

    fix Displaying users
    can now display "users" arr in json object

--- fetch-exempel-2.js
+++ fetch-exempel-2.js
@@ -1,27 +1,28 @@
 function createNode(element) {
-    return document.createElement(element);
+   return document.createElement(element);
 }
 
 function append(parent, el) {
-    return parent.appendChild(el);
-}
+    return parent.appendChild(el);}
 
 const ul = document.querySelector('#todos');
 const url = 'data/data.json';
 fetch(url)
     .then((resp) => resp.json())
     .then(function(data) {
-        console.log(data.todos);
-        console.log("Visa första i json-objektet: " + data.todos[0].todoTitle);
-        let todos = data.todos;
+        console.log(data.users);
+        console.log("Visa första i json-objektet: " + data.users[0].userName);
+        let users = data.users;
 
         // Returnerar json-bjekten genom att skapa en ny array med map()
-        return todos.map(function(todo) {
+        return users.map(function(user) {
             let li = createNode('li');
-            li.innerHTML = todo.todoTitle + " " + todo.todoDate;
+            li.innerHTML = user.userName + " " + user.userPassword;
             append(ul, li);
         })
     })
     .catch(function(error) {
         console.log(error);
     });
+
+  
